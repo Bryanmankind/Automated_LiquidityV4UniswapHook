b7e8e3d87840bfde993ef5e9330eeb7be4f5208b
Bryanmankind <shegeemankind@gmail.com>
2025-10-22 17:01:32 +0100

    poolCreation

--- src/v4Pool.sol/liquidityPool.sol
+++ src/v4Pool.sol/liquidityPool.sol
@@ -12,37 +12,26 @@ contract LiquidityPool {
 
     Currency public token0;
     Currency public token1;
-    uint24 public lpFee; 
-    int24 public tickSpacing;
 
     address public manager = 0x1F98431c8aD98523631AE4a59f267346ea31F984; // Uniswap V4 Pool Manager address
     uint160 public startingPrice = 79228162514264337593543950336; // 1:1 price ratio in Q64.96 format
 
     event PoolCreated(bytes32 indexed poolId, int24 initialTick, address hookContract);
 
-    constructor(
-        address _token0,
-        address _token1,
-        uint24 _lpFee,
-        int24 _tickSpacing
-    ) {
+
+    function createPool(address _token0, address _token1, uint24 _lpFee, int24 _tickSpacing, address hookContract) external returns (int24 tick) {
+
         token0 = Currency.wrap(_token0);
         token1 = Currency.wrap(_token1);
-        lpFee = _lpFee;
-        tickSpacing = _tickSpacing;
-    }
-
-
-    function createPool(address hookContract) external returns (int24 tick) {
-
+    
         // Sort tokens
         (token0, token1) = token0 < token1 ? (token0, token1) : (token1, token0);
 
         PoolKey memory pool = PoolKey({
         currency0: token0,
         currency1: token1,
-        fee: lpFee,
-        tickSpacing: tickSpacing,
+        fee: _lpFee,
+        tickSpacing: _tickSpacing,
         hooks: IHooks(hookContract)
      });
 
